03ca89109cb9338cb1aad6f9a74f4304b0ff7f75
victrolaface <rlovelett@gmail.com>
2026-06-24 17:34:48 -0500

    refactor
    refactored monster data into monster controller

--- game/scripts/data/monster.gd
+++ game/scripts/data/monster.gd
@@ -2,3 +2,13 @@ class_name Monster
 
 var species: SpeciesResource
 var hp: int
+var nickname: String
+
+var max_hp: int:
+	get: return species.max_hp
+	
+var image: Texture2D:
+	get: return species.image
+	
+var name: String:
+	get: return nickname if nickname else species.name
