169dab810e456161ee2efddaa150f1f6d5c4e340
victrolaface <rlovelett@gmail.com>
2026-07-05 16:36:57 -0500

    condition functionality

--- game/scripts/content/stat_modifier.gd
+++ game/scripts/content/stat_modifier.gd
@@ -1,4 +1,6 @@
 class_name StatModifier extends Resource
 
 @export var stat: Stat.Code
-@export var stat_modifiers: Array[StatModifier]
+@export var modifier: int
+
+#@export var stat_modifiers: Array[StatModifier]
