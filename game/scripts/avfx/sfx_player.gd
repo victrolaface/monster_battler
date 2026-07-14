08e53a39039c1410c80528790aee360401b37827
victrolaface <rlovelett@gmail.com>
2026-07-14 15:01:30 -0500

    fixed sfx player bug

--- game/scripts/avfx/sfx_player.gd
+++ game/scripts/avfx/sfx_player.gd
@@ -3,7 +3,7 @@ extends AudioStreamPlayer2D
 func _ready():
 	Events.on_avfx_sfx.connect(play_sfx)
 	
-func play_sfx(clip: AudioStream, instance: AVFXInstance):
+func play_sfx(instance: AVFXInstance, clip: AudioStream):
 	pitch_scale = randf_range(0.9, 1.1)
 	stream = clip
 	play()
