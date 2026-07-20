3408c012bc1a643bdbaf1dea67378aa96d5b24a0
victrolaface <16891257+victrolaface@users.noreply.github.com>
2026-07-20 17:22:46 -0500

    projectile fx

--- game/scripts/avfx/avfx_resources/avfx_projectile.gd
+++ game/scripts/avfx/avfx_resources/avfx_projectile.gd
@@ -1,6 +1,7 @@
 class_name AVFXProjectile extends AVFXResource
 
 @export var sprite: Texture2D
+@export var offset: Vector2 = Vector2(32,32)
 
 func _do(instance: AVFXInstance):
-	Events.on_avfx_sfx_projectile.emit(instance, sprite)
+	Events.on_avfx_projectile.emit(instance, sprite)
--- game/scripts/ui/battle_renderer.gd
+++ game/scripts/ui/battle_renderer.gd
@@ -14,8 +14,19 @@ func _ready():
 	Events.on_avfx_projectile.connect(avfx_projectile)
 	Events.on_ui_ready.emit()
 	
-func avfx_projectile(instance: AVFXInstance, sprite: Texture2D):
-	var sprite_frame = Sprite2D.new()
-	add_child(sprite_frame)
-	sprite_frame.texture = sprite
+func avfx_projectile(instance: AVFXInstance, texture: Texture2D):
+	var sprite = Sprite2D.new()
+	add_child(sprite)
+	sprite.texture = texture
+	
+	sprite.global_position = player_mon_module.frame.global_position
+	sprite.offset = instance.resource.offset
+	
+	var tween = get_tree().create_tween()
+	tween.tween_property(sprite, "global_position", enemy_mon_module.frame, 0.5)
+	tween.tween_callback(func(): cleanup_projectile(instance, sprite))
 	return
+
+func cleanup_projectile(instance, sprite):
+	sprite.queue_free()
+	instance.finish()
